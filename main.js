835c241ea0bd0d00237257bd1d09062ea6ef9277
Juan <142837433+JuanHenrique123@users.noreply.github.com>
2024-09-23 11:51:50 -0300

    f

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function multiplication(num1,num2){
 }
 console.log("a aréa do quadrado é", multiplication(6,6))
 
-let areaTriangulo = function(){
+let areaTriangulo = function(a,b){
     return 6*6/2;
  }
- console.log ("ar")
+ console.log ("área do triângulo é", areaTriangulo))
