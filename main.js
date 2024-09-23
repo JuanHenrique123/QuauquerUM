96dcca8ac1aa98ae9d2d3ee1cd06da05495c93f8
Juan <142837433+JuanHenrique123@users.noreply.github.com>
2024-09-23 11:41:24 -0300

    j

--- main.js
+++ main.js
@@ -13,4 +13,4 @@ function multiplication(num1,num2){
     return num1*num2;
 
 }
-console.log("a soma dos lados é", multiplication(15,15))
+console.log("a aréa do quadrado é", multiplication(15,15))
